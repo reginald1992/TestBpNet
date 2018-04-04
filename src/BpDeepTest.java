cc75898ea5dc7c6b60bbbfa4d8ff0b85f94bdcf4
Shulin <shulinliu@aliyun.com>
2018-04-04 12:59:40 +0800

    BP network prediction model

--- src/BpDeepTest.java
+++ src/BpDeepTest.java
@@ -1,30 +1,76 @@
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
+
 public class BpDeepTest{
     public static void main(String[] args){
         //初始化神经网络的基本配置
-        //第一个参数是一个整型数组，表示神经网络的层数和每层节点数，比如{3,10,10,10,10,2}表示输入层是3个节点，输出层是2个节点，中间有4层隐含层，每层10个节点
+        //第一个参数是一个整型数组，表示神经网络的层数和每层节点数，inputNodeNum：输入层节点数；outputNodeNum:输出层节点数
+        // 比如{20,10,10,10,5}表示输入层是20个节点,输出层是5个节点，中间有3层隐含层，每层10个节点
         //第二个参数是学习步长，第三个参数是动量系数
-        BpDeep bp = new BpDeep(new int[]{2,10,1}, 0.15, 0.8);
+        int inputNodeNum = 20;
+        int outputNodeNum = 5;
+        BpDeep bp = new BpDeep(new int[]{inputNodeNum,10,10,outputNodeNum}, 0.15, 0.8);
 
-        //设置样本数据，对应上面的4个二维坐标数据
-        double[][] data = new double[][]{{1,2},{2,2},{1,1},{2,1}};
-        //设置目标数据，对应4个坐标数据的分类
-        double[][] target = new double[][]{{1,0},{0,1},{0,1},{1,0}};
+        //数据读取并分割成训练集与预测集
+        Path path = Paths.get("./data/", "cv.csv");
+        File file = path.toFile();
+        try
+                (
+                        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)))
+                )
+        {
+            String line = null;
+            ArrayList<Double> al=new ArrayList<Double>();
+            while ((line = br.readLine()) != null)
+            {
+                al.add(Double.parseDouble(line));
+            }
+            double [] data = new double[al.size()];
+            for (int i = 0; i < data.length; ++i)
+            {
+                data[i] = al.get(i);
+            }
+
+//            overlap 数据分割间隔
+//            设置样本数据：train
+//            设置目标数据：target
+            int overlap = inputNodeNum + outputNodeNum;
+            double[][] train = new double[data.length/overlap][inputNodeNum];
+            double[][] target = new double[data.length/overlap][outputNodeNum];
+            for(int i = 0; i<data.length/overlap*overlap; i += overlap) {
+                for(int j = i; j<i+20; j++) {
+                    train[i/25][j%20] = data[j];
+                }
+                for(int j = i+20;j<i+25;j++) {
+                    target[i/25][(j-20)%5] = data[j];
+                }
+            }
 
         //迭代训练5000次
         for(int n=0;n<5000;n++)
-            for(int i=0;i<data.length;i++)
-                bp.train(data[i], target[i]);
+            for(int i=0;i<train.length;i++)
+                bp.train(train[i], target[i]);
 
         //根据训练结果来检验样本数据
-        for(int j=0;j<data.length;j++){
-            double[] result = bp.computeOut(data[j]);
-            System.out.println(Arrays.toString(data[j])+":"+Arrays.toString(result));
+        for(int j=0;j<train.length;j++){
+            double[] result = bp.computeOut(train[j]);
+            System.out.println(Arrays.toString(train[j])+":"+Arrays.toString(result));
         }
 
+//            double[][] dataTemp = new double[data.length/inputNodeNum][20];
+//            for(int i = 0;i<data.length/inputNodeNum*20;i++) {
+//                dataTemp[i/20][i%20] = data[i];
+//            }
+
         //根据训练结果来预测一条新数据的分类
-        double[] x = new double[]{3,1};
-        double[] result = bp.computeOut(x);
-        System.out.println(Arrays.toString(x)+":"+Arrays.toString(result));
+//        double[] x = new double[]{0.5};
+//        double[] result = bp.computeOut(x);
+//        System.out.println(Arrays.toString(x)+":"+Arrays.toString(result));
+    } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
